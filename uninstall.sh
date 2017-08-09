344ca37ba67222b7935d64d955b8e3acbfb73dca
llgoncalves <luanlg.cco@gmail.com>
2017-08-09 12:41:23 -0300

    Fixing uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,7 @@
 python setup.py install --record files.txt
 cat files.txt | xargs rm -rf
 rm -rf files.txt
-rm -rf /usr/share/mosaicode
 rm -rf build
 rm -rf dist
-rm -rf ~/mosaicode/configuration.xml
+rm -rf mosaicode_javascript_webaudio.egg-info
+rm -rf usr/local/lib/python2.7/dist-packages/mosaicode_javascript_webaudio-1.0-py2.7.egg
