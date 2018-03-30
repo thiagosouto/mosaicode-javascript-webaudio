385f09e14b62840fe1c47c4fd50b25faa49ecaed
Flavio Schiavoni <fls@rendera.com.br>
2018-03-30 00:48:26 -0300

    Compatibility with new version

--- mosaicode_lib_javascript_webaudio/extensions/ports/bool.py
+++ mosaicode_lib_javascript_webaudio/extensions/ports/bool.py
@@ -5,8 +5,8 @@ class Bool(Port):
     def __init__(self):
         Port.__init__(self)
         self.language = "javascript"
-        self.label = "BOOL"
+        self.hint = "BOOL"
         self.color = "#2c6300"
         self.multiple = True
         self.code = "$output$.push($input$);\n"
-        self.var_name = "block_$id$_$conn_type$$port_number$"
+        self.var_name = "$block[label]$_$block[id]$_$port[name]$"
